d7a546f68ba2359bd47be37f3c374d1acc7d9fdb
Larry Schirmer <larryschirmer@Larrys-MacBook-Pro.local>
2018-06-19 22:19:56 -0400

    add Card component and styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Wrapper, Name, Date } from './styles';
+import { combineName, formatDate } from '../../reducers/helperFunctions';
 
-const Card = () => {
+const Card = ({ name, creationDate }) => {
+  const combinedName = combineName(name);
+  const formatedDate = formatDate(creationDate);
+
   return (
-    <div>
-      <span>Hello World</span>
-    </div>
+    <Wrapper>
+      <Name>{combinedName}</Name>
+      <Date>{formatedDate}</Date>
+    </Wrapper>
   );
+};
+
+Card.propTypes = {
+  name: PropTypes.shape({
+    first: PropTypes.string.isRequired,
+    middle: PropTypes.string,
+    last: PropTypes.string.isRequired,
+  }),
+  creationDate: PropTypes.number.isRequired,
 };
 
 export default Card;
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,1 +1,22 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 0px;
+  padding: 10px;
+  background: #ddd;
+  border-radius: 5px;
+`;
+
+export const Name = styled.div`
+  font-size: 24px;
+  font-weight: 500;
+`;
+
+export const Date = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+`;
