58bb9262788347b3afb1b5568188a14af669a4aa
Larry Schirmer <larryschirmer@Larrys-MacBook-Pro.local>
2018-06-19 22:51:02 -0400

    add Tray component and styles

--- src/components/Tray/Tray.js
+++ src/components/Tray/Tray.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import { string, shape, number, arrayOf } from 'prop-types';
+import { Wrapper } from './styles';
+import { connect } from 'react-redux';
+
+import Sort from '../Sort';
+import Card from '../Card';
 
 class Tray extends Component {
   render() {
+    const { data, type, direction } = this.props;
+
+    const sortProps = { data, type, direction, Card };
+
     return (
-      <div>
-        <span>Hello World</span>
-      </div>
+      <Wrapper>
+        <Sort {...sortProps} />
+      </Wrapper>
     );
   }
 }
 
-export default Tray;
+Tray.propTypes = {
+  data: arrayOf(
+    shape({
+      id: string.isRequired,
+      name: shape({
+        first: string.isRequired,
+        middle: string,
+        last: string.isRequired,
+      }),
+      creationDate: number.isRequired,
+    }),
+  ),
+  type: shape({
+    id: string.isRequired,
+    text: string.isRequired,
+  }),
+  direction: shape({
+    id: string.isRequired,
+    text: string.isRequired,
+  }),
+};
+
+export function mapStateToProps({ api, ui }) {
+  const { data } = api;
+  const { type, direction } = ui;
+  return { data, type, direction };
+}
+
+export default connect(mapStateToProps)(Tray);
--- src/components/Tray/styles.js
+++ src/components/Tray/styles.js
@@ -1,1 +1,7 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-width: 750px;
+  max-width: 900px;
+  margin: 10px auto;
+`;
