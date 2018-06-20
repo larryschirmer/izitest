a524896686d778535f281a75399d7c9358de083b
Larry Schirmer <larryschirmer@Larrys-MacBook-Pro.local>
2018-06-19 22:40:48 -0400

    add Header component and styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
+import { string, shape } from 'prop-types';
+import { connect } from 'react-redux';
+import { Wrapper, Logo, Navigation } from './styles';
+import Button from '../Button';
+import { fetchFromAPI, updateType, updateDirection } from '../../reducers/actions';
 
 class Header extends Component {
+  state = {
+    sortType: 'Sort by Creation Date',
+    sortDirection: 'Sort Assending',
+    fetchState: 'ready',
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (state === props) return null;
+
+    return {
+      fetchState: props.fetchState,
+      sortType: props.type.text,
+      sortDirection: props.direction.text,
+    };
+  }
+
   render() {
+    const { sortType, sortDirection, fetchState } = this.state;
+    const { fetchFromAPI, updateType, updateDirection } = this.props;
+
     return (
-      <div>
-        <span>Hello World</span>
-      </div>
+      <Wrapper>
+        <Logo />
+        <Navigation>
+          <Button label={sortType} handleClick={updateType} />
+          <Button label={sortDirection} handleClick={updateDirection} />
+          <Button label="Fetch Data" state={fetchState} handleClick={fetchFromAPI} />
+        </Navigation>
+      </Wrapper>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  fetchState: string.isRequired,
+  type: shape({
+    id: string.isRequired,
+    text: string.isRequired,
+  }),
+  direction: shape({
+    id: string.isRequired,
+    text: string.isRequired,
+  }),
+};
+
+export function mapStateToProps({ api, ui }) {
+  const { fetchState } = api;
+  const { type, direction } = ui;
+  return { fetchState, type, direction };
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    fetchFromAPI: () => dispatch(fetchFromAPI()),
+    updateType: () => dispatch(updateType()),
+    updateDirection: () => dispatch(updateDirection()),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Header);
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,1 +1,31 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 100px auto;
+  min-width: 750px;
+  max-width: 900px;
+  height: 50px;
+  margin: 10px auto;
+  background: #ddd;
+  border-radius: 5px;
+`;
+
+export const Logo = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
+  border-radius: 5px;
+
+  grid-column: 1/2;
+  margin: auto;
+`;
+
+export const Navigation = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  height: 100%;
+  width: 100%;
+  grid-column: 2/3;
+`;
